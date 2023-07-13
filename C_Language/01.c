ef90b187c827280c6ac0c40a82258c315a5c0774
darshan <darshan.patel.tops@gmail.com>
2023-07-13 15:13:56 +0530

    first

---
+++ C_Language/01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+// include is used to used include the file
+// stdio   standard input output (this is a library)
+//.h is a header file
+
+
+// any c program execution starts from main function
+
+//main is a user defined function
+main()
+{
+	
+	printf("Hello welcome to c language ");
+
+/*printf(" idhar jo bhi likhna ho");*/
+
+}
+
---
+++ C_Language/02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	//int is a datatype
+	//number is a variable
+	// variable is the name of the memory allocation
+	printf("learn about input \n");
+	
+	int number1;
+	//Address of variable for &
+	scanf("%d",&number1);
+	printf("%d",number1);
+	
+}
