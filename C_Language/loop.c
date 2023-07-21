8dddf88623b7e84e2581c9f6e4179beda1e70090
darshan <darshan.patel.tops@gmail.com>
2023-07-21 09:49:29 +0530

    first

---
+++ C_Language/loop.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+main()
+{
+	
+	
+	/*
+	
+	i = intilization/start
+	c = condition
+	u = updation
+	
+	
+	
+	for(i;c;u)
+	{
+		statement
+	}
+	
+	
+	*/
+//	printf("welcome to loop");
+	
+	int i;
+	for(i=0;i<=5;i++)
+	{
+	
+		printf("Pizza %d \n",i);
+		scanf("%d",)
+	}
+	
+	
+}
