798bea8656402708e640b434785d699edb5ede16
darshan <darshan.patel.tops@gmail.com>
2023-08-05 14:41:42 +0530

    update

---
+++ cpp language/oops3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class classname
+{
+	
+	
+//	Access Modifier
+
+//	public:		It is like Water / Bicycle
+//	protected:	It is like Juice / Bike
+//	private:	It is like Expensive Wine/Black Water / Rolls Royce	
+
+		public :
+//		protected :
+//		private :
+		void call()
+		{
+			cout<<"Gathiya"<<endl;
+		}
+
+
+
+
+
+};
+
+
+main()
+{
+	classname cn;
+	cn.call();
+		
+}
