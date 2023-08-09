dc05bb6370d4e7a295e31a6b55b5fd1b46871d60
darshan <darshan.patel.tops@gmail.com>
2023-08-09 08:31:51 +0530

    update

--- cpp language/oops7.cpp
+++ cpp language/oops7.cpp
@@ -1,65 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Scope resolution ::
-//it is used to increase the scope of the function
-class dada
-{
-	public :
-		int zameen()
-		{
-			cout<<"Zameen"<<endl;
-		}
+class dada {
+public:
+    int zameen() {
+        cout << "Zameen" << endl;
+    }
 };
 
-class kaka : public dada
-{
-	public :
-//		using dada::zameen;
-		void car()
-		{
-//dada::zameen();
-			cout<<"Car"<<endl;
-		}
+class kaka : public dada {
+public:
+    void car() {
+        cout << "Car" << endl;
+    }
 };
 
-class papa : public dada
-{
-	public :
-		using dada::zameen;
-//		dada::zameen();
-		void house()
-		{
-		dada::zameen();
-			cout<<"House"<<endl;
-		}
-};
-class beta : public papa,public kaka
-{
-	public :
-		
-//		using dada::zameen;
-//		using dada::zameen;
-//		dada::zameen();
-		void bike()
-		{
-			
-			cout<<"Bike"<<endl;
-		}
+class papa : public dada {
+public:
+    void house() {
+        cout << "House" << endl;
+    }
 };
 
-main()
-{
-	
-	beta b;
-	b.bike();
-	b.car();
-	b.house();
-//	b.zameen();
-//using dada::zameen;
-//dada::zameen();
-dada d;
-d.zameen();
-	
-	
+class beta : public papa, public kaka {
+public:
+    void bike() {
+        cout << "Bike" << endl;
+    }
+};
+
+int main() {
+    beta b;
+    b.bike();
+    b.car();
+    b.house();
+    b.papa::zameen();  // Specify which base class's zameen() to call
 }
